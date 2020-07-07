6aa5c9ccca2de3eb8faa89504891d087494d0336
John Hunt <john.hunt10@gmail.com>
2020-07-07 17:12:38 +0100

    Removed trailing ';'

--- HelloWorld/app/src/main/java/com/jjh/android/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/jjh/android/helloworld/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        lifecycle.addObserver(MyLifecycleObserver());
+        lifecycle.addObserver(MyLifecycleObserver())
 
         Log.d("MainActivity Lifecycle", "onCreate()")
 
