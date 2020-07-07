3c97ce2d957f06973311f8733298526594137ef1
John Hunt <john.hunt10@gmail.com>
2020-07-07 16:59:33 +0100

    Removed unused import

--- FileDemoApp/app/src/main/java/com/jjh/android/filedemoapp/MainActivity.kt
+++ FileDemoApp/app/src/main/java/com/jjh/android/filedemoapp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.jjh.android.filedemoapp
 import android.content.Context
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
