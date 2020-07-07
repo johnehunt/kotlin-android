48fbec01c2deab1d12917531ff4f0f8b81b34589
John Hunt <john.hunt10@gmail.com>
2020-07-07 11:56:13 +0100

    Simple Service Example

---
+++ ServiceDemo/app/src/main/java/com/jjh/android/servicedemo/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.jjh.android.servicedemo
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    private var message: TextView? = null
+    private var startServiceIntent: Intent? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        message = findViewById(R.id.message)
+    }
+
+    // Button handler methods
+    fun onStartButtonClick(v: View?) {
+        try {
+            Log.d(TAG, "Starting service!")
+            startServiceIntent = Intent(this, SampleService::class.java)
+            startService(startServiceIntent)
+            message!!.text = "Started Service"
+        } catch (e: Exception) {
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun onStopButtonClick(v: View?) {
+        try {
+            Log.d(TAG, "Stopping service!")
+            stopService(startServiceIntent)
+            message!!.text = "Stopped Service"
+        } catch (e: Exception) {
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
---
+++ ServiceDemo/app/src/main/java/com/jjh/android/servicedemo/SampleService.kt
@@ -0,0 +1,35 @@
+package com.jjh.android.servicedemo
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+class SampleService : Service() {
+
+    companion object {
+        private const val TAG = "SampleService"
+    }
+
+    // Have to implement as abstract method in Service
+    override fun onBind(intent: Intent): IBinder? {
+        Log.d(TAG, "onBind()")
+        return null // Must return null for a standard started service
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(TAG, "onCreate()")
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        Log.d(TAG, "onStartCommand()")
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy()")
+    }
+
+}
