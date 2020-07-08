22ea4d1f4d98531b9edc3fe54a9747f51ca4564f
John Hunt <john.hunt10@gmail.com>
2020-07-08 09:57:20 +0100

    Added companion object to activity to handle tag for logging

--- Activities/app/src/main/java/com/jjh/android/activities/MainActivity.kt
+++ Activities/app/src/main/java/com/jjh/android/activities/MainActivity.kt
@@ -2,10 +2,18 @@ package com.jjh.android.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity Lifecycle"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate()")
     }
+
 }
