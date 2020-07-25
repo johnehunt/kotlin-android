66331eaafc06b2ad72f3e4a55985fcc0966c7944
John Hunt <john.hunt10@gmail.com>
2020-07-25 13:25:05 +0100

    Removed unused import

--- TableLayoutDemo/app/src/main/java/com/jjh/android/tablelayoutdemo/MainActivity.kt
+++ TableLayoutDemo/app/src/main/java/com/jjh/android/tablelayoutdemo/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
-import android.widget.TextView
 import com.jjh.android.tablelayoutdemo.model.Product
 
 class MainActivity : AppCompatActivity() {
