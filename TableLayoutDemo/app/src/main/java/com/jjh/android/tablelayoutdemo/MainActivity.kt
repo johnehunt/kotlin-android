459ae355feaa2bdba14218f8e83e74e5bd02df4e
John Hunt <john.hunt10@gmail.com>
2020-07-25 13:24:20 +0100

    TableLayout and Table Row demo

---
+++ TableLayoutDemo/app/src/main/java/com/jjh/android/tablelayoutdemo/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.jjh.android.tablelayoutdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import com.jjh.android.tablelayoutdemo.model.Product
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    private lateinit var id: EditText
+    private lateinit var productName: EditText
+    private lateinit var productQuantity: EditText
+    private lateinit var productsView: EditText
+
+    private val products: MutableList<Product> = mutableListOf<Product>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d(TAG, "onCreate")
+        setContentView(R.layout.activity_main)
+
+        id = findViewById(R.id.productId)
+        productName = findViewById(R.id.productName)
+        productQuantity = findViewById(R.id.productQuantity)
+        productsView = findViewById(R.id.productsView)
+        productsView.isEnabled = false // Make the text field non editable
+
+    }
+
+    fun addProduct(view: View) {
+        Log.d(TAG, "addProduct")
+        val pId = id.text.toString()
+        val pName = productName.text.toString()
+        val pQuantity = productQuantity.text.toString()
+        if (pId != "") {
+            val product = Product(pId, pName, Integer.parseInt(pQuantity))
+            products.add(product)
+            clearFields()
+            updateProductsView()
+        } else {
+            id.setText("Requires an ID")
+        }
+    }
+
+    fun findProduct(view: View) {
+        Log.d(TAG, "findProduct")
+    }
+
+    fun deleteProduct(view: View) {
+        Log.d(TAG, "deleteProduct")
+    }
+
+    private fun clearFields() {
+        Log.d(TAG, "clearFields")
+        id.setText("")
+        productName.setText("")
+        productQuantity.setText("")
+    }
+
+    private fun updateProductsView() {
+        Log.d(TAG, "updateProductsView")
+        Log.d(TAG, products.toString())
+        productsView.setText("")
+        productsView.setText(products.toString())
+    }
+
+}
---
+++ TableLayoutDemo/app/src/main/java/com/jjh/android/tablelayoutdemo/model/Product.kt
@@ -0,0 +1,3 @@
+package com.jjh.android.tablelayoutdemo.model
+
+data class Product(val id: String, val name: String, val quantity: Int)
