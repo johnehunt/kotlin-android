341325e8a0009bb0981636ce85777f51dee40825
John Hunt <john.hunt10@gmail.com>
2020-08-04 11:44:47 +0100

    Added us eof synthetic variables

--- IntentDemo1/app/src/main/java/com/jjh/android/intentdemo1/MainActivity.kt
+++ IntentDemo1/app/src/main/java/com/jjh/android/intentdemo1/MainActivity.kt
@@ -5,24 +5,19 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private var diallerText: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Initialize dialier text field field
-        diallerText = findViewById(R.id.editText)
     }
 
     // Handler method for DIal Button
     fun onDialButtonClick(v: View?) {
-        val myData = diallerText!!.text.toString()
+        val myData = dialerText.text.toString()
         // Creates an Intent to trigger dialler
         val intent = Intent(Intent.ACTION_DIAL, Uri.parse(myData))
         startActivity(intent)
