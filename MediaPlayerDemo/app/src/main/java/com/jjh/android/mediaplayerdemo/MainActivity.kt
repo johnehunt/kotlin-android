1e79b0161b591ca7abada9d977ae2206871e1966
John Hunt <john.hunt10@gmail.com>
2020-08-04 11:51:32 +0100

    Updated for Gradle

--- MediaPlayerDemo/app/src/main/java/com/jjh/android/mediaplayerdemo/MainActivity.kt
+++ MediaPlayerDemo/app/src/main/java/com/jjh/android/mediaplayerdemo/MainActivity.kt
@@ -6,30 +6,22 @@ import android.media.MediaPlayer.OnPreparedListener
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var pause: Button? = null
-    var play: Button? = null
-    var stop: Button? = null
-    var mediaPlayer: MediaPlayer? = null
+    private lateinit var mediaPlayer: MediaPlayer
 
     // Lifecycle Methods
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Setup button references
-        pause = findViewById(R.id.pause)
-        play = findViewById(R.id.play)
-        stop = findViewById(R.id.stop)
-
         // Determine which buttons are enabled at start
-        pause?.setEnabled(false)
-        stop?.setEnabled(false)
+        pause.isEnabled = false
+        stop.isEnabled = false
 
         // Setup Media Player - note must manually add raw folder
         // under the res folder in your project
@@ -37,19 +29,18 @@ class MainActivity : AppCompatActivity() {
         mediaPlayer = MediaPlayer.create(this, R.raw.song)
 
         // Set up callback listeners
-        mediaPlayer!!.setOnErrorListener(CustomMediaPlayerOnErrorListener())
-        mediaPlayer!!.setOnInfoListener(CustomMediaPlayerInfoListener())
-        mediaPlayer!!.setOnPreparedListener(CustomerMediaPlayerOnPreparedListener())
-        mediaPlayer!!.setOnCompletionListener(CustomerMediaPlayerOnCompletionListener())
+        mediaPlayer.setOnErrorListener(CustomMediaPlayerOnErrorListener())
+        mediaPlayer.setOnInfoListener(CustomMediaPlayerInfoListener())
+        mediaPlayer.setOnPreparedListener(CustomerMediaPlayerOnPreparedListener())
+        mediaPlayer.setOnCompletionListener(CustomerMediaPlayerOnCompletionListener())
     }
 
     override fun onDestroy() {
         try {
-            if (mediaPlayer!!.isPlaying) {
-                mediaPlayer!!.stop()
+            if (mediaPlayer.isPlaying) {
+                mediaPlayer.stop()
             }
-            mediaPlayer!!.release()
-            mediaPlayer = null
+            mediaPlayer.release()
         } catch (exp: Exception) {
             Toast.makeText(
                 applicationContext,
@@ -65,7 +56,7 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(applicationContext, "Playing sound", Toast.LENGTH_SHORT).show()
         try {
             // Can throw IllegalStateException if it is called in an invalid state
-            mediaPlayer!!.start()
+            mediaPlayer.start()
         } catch (exp: IllegalStateException) {
             Toast.makeText(
                 applicationContext,
@@ -75,28 +66,28 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Enable / Disable appropriate buttons
-        pause!!.isEnabled = true
-        play!!.isEnabled = false
-        stop!!.isEnabled = true
+        pause.isEnabled = true
+        play.isEnabled = false
+        stop.isEnabled = true
     }
 
     fun onPauseButtonClick(v: View?) {
         Toast.makeText(applicationContext, "Pausing sound", Toast.LENGTH_SHORT).show()
-        mediaPlayer!!.pause()
+        mediaPlayer.pause()
 
         // Enable / Disable buttons
-        pause!!.isEnabled = false
-        play!!.isEnabled = true
-        stop!!.isEnabled = false
+        pause.isEnabled = false
+        play.isEnabled = true
+        stop.isEnabled = false
     }
 
     fun onStopButtonClick(v: View?) {
         Toast.makeText(applicationContext, "Stopping sound", Toast.LENGTH_SHORT).show()
         try {
-            mediaPlayer!!.stop()
+            mediaPlayer.stop()
             // Need to prepare mediaPlayer
             // so that it can play another tune
-            mediaPlayer!!.prepare()
+            mediaPlayer.prepare()
         } catch (exp: Exception) {
             Toast.makeText(
                 applicationContext,
@@ -106,9 +97,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Enable / Disable buttons
-        pause!!.isEnabled = false
-        play!!.isEnabled = true
-        stop!!.isEnabled = false
+        pause.isEnabled = false
+        play.isEnabled = true
+        stop.isEnabled = false
     }
     // Media Player callback listeners
     /**
@@ -163,9 +154,9 @@ class MainActivity : AppCompatActivity() {
         OnCompletionListener {
         override fun onCompletion(mp: MediaPlayer) {
             Log.e("CustomerMediaPlayerOnCompletionListener", mp.toString())
-            play!!.isEnabled = true
-            pause!!.isEnabled = false
-            stop!!.isEnabled = false
+            play.isEnabled = true
+            pause.isEnabled = false
+            stop.isEnabled = false
         }
     }
 }
