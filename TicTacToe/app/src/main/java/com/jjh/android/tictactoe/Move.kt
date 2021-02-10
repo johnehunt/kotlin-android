2f46abdcc6635e587c5009ea7be3f961f24824c6
John Hunt <john.hunt10@gmail.com>
2021-02-10 14:19:19 +0000

    Changed to data class

--- TicTacToe/app/src/main/java/com/jjh/android/tictactoe/Move.kt
+++ TicTacToe/app/src/main/java/com/jjh/android/tictactoe/Move.kt
@@ -1,7 +1,3 @@
 package com.jjh.android.tictactoe
 
-class Move(val x: Int, val y: Int, val counter: Counter) {
-
-    override fun toString() = "Move($x, $y: $counter)"
-
-}
+data class Move(val x: Int, val y: Int, val counter: Counter)
