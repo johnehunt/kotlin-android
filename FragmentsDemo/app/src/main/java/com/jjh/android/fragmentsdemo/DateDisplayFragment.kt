53006d77815ed2ef8349b56a9d7bff069f8b2224
John Hunt <john.hunt10@gmail.com>
2021-02-11 11:36:25 +0000

    Updated for Kotlin 1.4.30

--- FragmentsDemo/app/src/main/java/com/jjh/android/fragmentsdemo/DateDisplayFragment.kt
+++ FragmentsDemo/app/src/main/java/com/jjh/android/fragmentsdemo/DateDisplayFragment.kt
@@ -4,50 +4,52 @@ import android.content.Context
 import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import java.util.*
 
-class DateTimeFragment : Fragment() {
-    private var time: String? = null
+import java.util.Date
 
-    override fun onCreate(state: Bundle?) {
-        super.onCreate(state)
-        if (time == null) {
-            time = SimpleDateFormat("dd MM yyyy HH:mm:ss").format(Date())
-        }
-        Log.d("DateTimeFragment", "onCreate()")
+import kotlinx.android.synthetic.main.fragment_date_display.*
+
+class DateDisplayFragment : Fragment() {
+    companion object {
+        private const val TAG = "DateDisplayFragment"
     }
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = TextView(this.activity)
-        view.text = time
-        Log.d("DateTimeFragment", "onCreateView()")
-        return view
+        Log.d(TAG, "onCreateView()")
+        return inflater.inflate(R.layout.fragment_date_display, container, false)
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        Log.d("DateTimeFragment", "onAttach()")
+        Log.d(TAG, "onAttach()")
+    }
+
+    override fun onCreate(state: Bundle?) {
+        super.onCreate(state)
+        Log.d(TAG, "onCreate()")
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d("DateTimeFragment", "onViewCreated()")
+        Log.d(TAG, "onViewCreated()")
+        val time = SimpleDateFormat("dd MM yyyy HH:mm:ss").format(Date())
+        textView.text = time
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        Log.d("DateTimeFragment", "onActivityCreated()")
+        Log.d(TAG, "onActivityCreated()")
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        Log.d("DateTimeFragment", "onDestroyView()")
+        Log.d(TAG, "onDestroyView()")
     }
+
 }
