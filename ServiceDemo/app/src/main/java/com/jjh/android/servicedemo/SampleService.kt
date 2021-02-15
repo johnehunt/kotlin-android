2f9f8b1daeabd2416c33fddc9f41767d02b6ceb3
John Hunt <john.hunt10@gmail.com>
2021-02-15 10:23:52 +0000

    Added comment on type of service

--- ServiceDemo/app/src/main/java/com/jjh/android/servicedemo/SampleService.kt
+++ ServiceDemo/app/src/main/java/com/jjh/android/servicedemo/SampleService.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import android.os.IBinder
 import android.util.Log
 
+/**
+ * Illustrates Standard Service
+ */
 class SampleService : Service() {
 
     companion object {
