470e3bfbe6166be718b97d9618cb273f4c16845b
John Hunt <john.hunt10@gmail.com>
2021-02-16 10:03:50 +0000

    Removed unnecessary curly brackets

--- DaggerDemo/app/src/main/java/com/jjh/android/daggerdemo/MainActivity.kt
+++ DaggerDemo/app/src/main/java/com/jjh/android/daggerdemo/MainActivity.kt
@@ -28,8 +28,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d(TAG, "onCreate - accessing injected objects")
-        Log.d(TAG, "onCreate - ${registrationModel}")
-        Log.d(TAG, "onCreate - ${statusManager}")
+        Log.d(TAG, "onCreate - $registrationModel")
+        Log.d(TAG, "onCreate - $statusManager")
 
         registrationModel.logMe()
 
