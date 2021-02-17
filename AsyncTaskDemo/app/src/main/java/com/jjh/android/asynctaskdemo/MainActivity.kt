ec6f4d4d7faa71029117e7642f641dcdc203d859
John Hunt <john.hunt10@gmail.com>
2021-02-17 11:30:09 +0000

    Updateds

--- AsyncTaskDemo/app/src/main/java/com/jjh/android/asynctaskdemo/MainActivity.kt
+++ AsyncTaskDemo/app/src/main/java/com/jjh/android/asynctaskdemo/MainActivity.kt
@@ -3,26 +3,23 @@ package com.jjh.android.asynctaskdemo
 import android.app.ProgressDialog
 import android.os.AsyncTask
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
 import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+import kotlinx.android.synthetic.main.activity_main.*
 
-    private var finalResult: TextView? = null
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val time = findViewById<EditText>(R.id.in_time)
-        val button = findViewById<Button>(R.id.btn_run)
-        finalResult = findViewById(R.id.tv_result)
-        button.setOnClickListener {
-            val runner = AsyncTaskRunner()
-            val sleepTime = time.getText().toString()
-            runner.execute(sleepTime)
-        }
+    }
+
+    fun onButtonClick(v: View) {
+        val runner = AsyncTaskRunner()
+        val sleepTime = inTime.text.toString()
+        runner.execute(sleepTime)
     }
 
     private inner class AsyncTaskRunner :
@@ -47,11 +44,11 @@ class MainActivity : AppCompatActivity() {
         override fun onPostExecute(result: String?) {
             // execution of result of Long time consuming operation
             progressDialog!!.dismiss()
-            finalResult!!.text = result
+            finalResult.text = result
         }
 
         override fun onPreExecute() {
-            val time = findViewById<EditText>(R.id.in_time)
+            val time = findViewById<EditText>(R.id.inTime)
             progressDialog = ProgressDialog.show(
                 this@MainActivity,
                 "ProgressDialog",
@@ -60,7 +57,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onProgressUpdate(vararg values: String?) {
-            finalResult!!.text = values[0]
+            finalResult.text = values[0]
         }
 
     }
