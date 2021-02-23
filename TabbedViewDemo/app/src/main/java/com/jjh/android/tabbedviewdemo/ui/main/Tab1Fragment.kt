69bd4e2f3440da76b6ee2736e3a69a17fa30b8c9
John Hunt <john.hunt10@gmail.com>
2021-02-23 16:47:03 +0000

    Removed unused imports

--- TabbedViewDemo/app/src/main/java/com/jjh/android/tabbedviewdemo/ui/main/Tab1Fragment.kt
+++ TabbedViewDemo/app/src/main/java/com/jjh/android/tabbedviewdemo/ui/main/Tab1Fragment.kt
@@ -5,10 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.jjh.android.tabbedviewdemo.R
 
 /**
--- TabbedViewDemo/app/src/main/java/com/jjh/android/tabbedviewdemo/ui/main/Tab2Fragment.kt
+++ TabbedViewDemo/app/src/main/java/com/jjh/android/tabbedviewdemo/ui/main/Tab2Fragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
 import com.jjh.android.tabbedviewdemo.R
 
 class Tab2Fragment : Fragment() {
