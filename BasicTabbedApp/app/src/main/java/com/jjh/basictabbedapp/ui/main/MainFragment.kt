8993a737b70f02d3d819d12b6ac63aec738c9267
John Hunt <john.hunt10@gmail.com>
2021-03-18 12:03:51 +0000

    Tidying

--- BasicTabbedApp/app/src/main/java/com/jjh/basictabbedapp/ui/main/MainFragment.kt
+++ BasicTabbedApp/app/src/main/java/com/jjh/basictabbedapp/ui/main/MainFragment.kt
@@ -1,6 +1,5 @@
 package com.jjh.basictabbedapp.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
